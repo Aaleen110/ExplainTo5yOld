90e56ec0021231a5847bc4037bbbd338202c6efa
Aaleen <aaleenmirza110@gmail.com>
2023-06-05 21:33:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import './App.css';
+
+const API_KEY = "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"
+function App() {
+
+  useEffect(() => {
+    askQuestion();
+  }, []);
+
+  async function askQuestion() {
+
+    const apiMessages = [{ "role": "user", "content": "What is a meteorite" }]
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [...apiMessages]
+    }
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    });
+
+    const answer = await response.json();
+
+
+    console.log("Hello", answer)
+
+
+  }
+
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/axios.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const networkService = axios.create({
+    baseURL:"https://api.themoviedb.org/3"
+});
+
+export default networkService;
