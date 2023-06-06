269a39207e356d77e5af1360489dded9608691a8
Aaleen <aaleenmirza110@gmail.com>
2023-06-06 18:28:42 +0530

    UI complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useEffect } from 'react';
 import './App.css';
+import pinwheel from './assets/svgs/pinwheel.svg'
+import sun from './assets/svgs/sun.svg'
+import mountain from './assets/svgs/mountain.svg'
+import tree from './assets/svgs/tree.svg'
+import cloud from './assets/svgs/cloud.svg'
+import goleft from './assets/svgs/goleft.svg'
+import goright from './assets/svgs/goright.svg'
+
 
 const API_KEY = "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"
 function App() {
 
   useEffect(() => {
-    askQuestion();
+    // askQuestion();
   }, []);
 
   async function askQuestion() {
@@ -36,7 +44,57 @@ function App() {
 
   return (
     <div className="App">
-      First ChatGPT Project
+      {/* SKY */}
+      <div className="sky-section">
+        <img className='sun' src={sun} alt="sun" />
+        <img className='mountain' src={mountain} alt="mountain" />
+        <img className='tree' src={tree} alt="tree" />
+
+        <img className='cloud-1' src={cloud} alt="cloud" />
+        <img className='cloud-2' src={cloud} alt="cloud" />
+        <img className='cloud-3' src={cloud} alt="cloud" />
+        <img className='cloud-4' src={cloud} alt="cloud" />
+        <div className='box-container'>
+          <h1 className='title'>Explain to a 5 year old</h1>
+
+          <div className='search-box-container'>
+            <input className='search-box'></input>
+            <button className='search-button'>Search!</button>
+          </div>
+        </div>
+      </div>
+
+      {/* GRASS */}
+      <div className="grass-section">
+        <div className="pin-wheel-container">
+          <img className='pin-wheel' src={pinwheel} alt="pinwheel" />
+          <div className='pin-wheel-line'></div>
+        </div>
+
+        <div className='result-tablet'>
+
+          <div style={{ diplay: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'relative', marginTop: 16 }}>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding:16, backgroundColor:"#eee" }}>
+
+              <img className='paw' src={goleft} alt="goleft" />
+              <div style={{ fontSize: 28, fontFamily: 'fantasy', }}>
+                A Meteor
+              </div>
+
+
+              <img className='paw' src={goright} alt="goright" />
+            </div>
+
+
+          </div>
+
+          <div style={{ display: 'flex', fontSize: 25, fontFamily: 'fantasy', alignSelf: 'center', padding: 20, overflow: 'scroll' }}>Think of them as “space rocks." When meteoroids enter Earth's atmosphere (or that of another planet, like Mars) at high speed and burn up, the fireballs or “shooting stars” are called meteors. When a meteoroid survives a trip through the atmosphere and hits the ground, it's called a meteorite.
+          </div>
+
+
+
+        </div>
+      </div>
     </div>
   );
 }
