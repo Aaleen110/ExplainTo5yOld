668d7c3003b5bfe6d6e9e99e13b61ebcfd2a6271
Aaleen <aaleenmirza110@gmail.com>
2023-06-07 14:59:21 +0530

    code improvements

--- src/App.js
+++ src/App.js
@@ -1,65 +1,83 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import pinwheel from './assets/svgs/pinwheel.svg'
-import sun from './assets/svgs/sun.svg'
-import mountain from './assets/svgs/mountain.svg'
-import tree from './assets/svgs/tree.svg'
-import cloud from './assets/svgs/cloud.svg'
-import goleft from './assets/svgs/goleft.svg'
-import goright from './assets/svgs/goright.svg'
+import { pinwheel, sun, mountain, tree, cloud, goleft, goright } from './assets/svgs'
+import Speech from 'react-speech';
 
+const API_KEY = "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";
 
-const API_KEY = "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"
 function App() {
+  const [query, setQuery] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     // askQuestion();
   }, []);
 
   async function askQuestion() {
+    if (query.length) {
+      setLoading(true)
+      const apiMessages = [{ "role": "user", "content": `Explain to a five year old, what is ${query}` }]
+      const apiRequestBody = {
+        "model": "gpt-3.5-turbo",
+        "messages": [...apiMessages]
+      }
 
-    const apiMessages = [{ "role": "user", "content": "What is a meteorite" }]
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + API_KEY,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(apiRequestBody)
+      }).catch(error => {
+        setLoading(false)
+        alert(error)
+      });
 
-    const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [...apiMessages]
+      const responseJSON = await response.json();
+      if (responseJSON && responseJSON.choices.length) {
+        let answer = responseJSON?.choices[0]?.message?.content;
+        setAnswer(answer)
+        setLoading(false)
+      } else {
+        alert('Something went wrong!')
+        setLoading(false)
+      }
     }
+  }
 
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + API_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody)
-    });
-
-    const answer = await response.json();
-
-
-    console.log("Hello", answer)
-
-
+  function reset() {
+    setAnswer("")
+    setQuery("")
   }
 
   return (
-    <div className="App">
+    <div className="container">
+      {loading ?
+        <div style={{ height: '100vh', width: '100vw', position: 'absolute', zIndex: 2, backgroundColor: '#000', opacity: 0.5, display: 'flex', justifyContent: 'center', alignItems: 'center' }}></div> : false}
+
+
+
       {/* SKY */}
       <div className="sky-section">
         <img className='sun' src={sun} alt="sun" />
         <img className='mountain' src={mountain} alt="mountain" />
         <img className='tree' src={tree} alt="tree" />
-
         <img className='cloud-1' src={cloud} alt="cloud" />
         <img className='cloud-2' src={cloud} alt="cloud" />
         <img className='cloud-3' src={cloud} alt="cloud" />
         <img className='cloud-4' src={cloud} alt="cloud" />
         <div className='box-container'>
           <h1 className='title'>Explain to a 5 year old</h1>
-
           <div className='search-box-container'>
-            <input className='search-box'></input>
-            <button className='search-button'>Search!</button>
+            <input
+              className='search-box'
+              type="text"
+              value={query}
+              onChange={(e) => { e.target.value.length ? setQuery(e.target.value) : reset() }}
+            ></input>
+            <button onClick={() => askQuestion()} className='search-button'>Search!</button>
           </div>
         </div>
       </div>
@@ -70,31 +88,27 @@ function App() {
           <img className='pin-wheel' src={pinwheel} alt="pinwheel" />
           <div className='pin-wheel-line'></div>
         </div>
-
         <div className='result-tablet'>
-
-          <div style={{ diplay: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', position: 'relative', marginTop: 16 }}>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', padding:16, backgroundColor:"#eee" }}>
-
+          <div className='result-box-header'>
+            <div className='result-box-inner'>
               <img className='paw' src={goleft} alt="goleft" />
-              <div style={{ fontSize: 28, fontFamily: 'fantasy', }}>
-                A Meteor
+              <div className='result-title'>
+                {answer.length ? query : ''}
               </div>
-
-
               <img className='paw' src={goright} alt="goright" />
             </div>
-
-
           </div>
-
-          <div style={{ display: 'flex', fontSize: 25, fontFamily: 'fantasy', alignSelf: 'center', padding: 20, overflow: 'scroll' }}>Think of them as “space rocks." When meteoroids enter Earth's atmosphere (or that of another planet, like Mars) at high speed and burn up, the fireballs or “shooting stars” are called meteors. When a meteoroid survives a trip through the atmosphere and hits the ground, it's called a meteorite.
-          </div>
-
-
-
+          <div className='result-box-body'>{query.length ? answer : ''}</div>
         </div>
       </div>
+
+      <Speech
+        stop={true}
+        pause={true}
+        resume={true}
+        text="I am displaying all buttons"
+        displayText="Hello"
+      />
     </div>
   );
 }
---
+++ src/assets/svgs/index.js
@@ -0,0 +1,11 @@
+import pinwheel from './pinwheel.svg'
+import sun from './sun.svg'
+import mountain from './mountain.svg'
+import tree from './tree.svg'
+import cloud from './cloud.svg'
+import goleft from './goleft.svg'
+import goright from './goright.svg'
+
+export {
+    pinwheel, sun, mountain, tree, cloud, goleft, goright
+}
