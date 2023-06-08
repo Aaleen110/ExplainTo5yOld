a2c52742d97efa419c0086a77e54ceac7bd3017a
Aaleen <aaleenmirza110@gmail.com>
2023-06-08 22:06:45 +0530

    updated gh pages link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [answer, setAnswer] = useState("");
   const [loading, setLoading] = useState(false)
 
-useEffect(() => {
+  useEffect(() => {
     // askQuestion();
   }, []);
 
